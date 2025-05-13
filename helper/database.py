1a9931f9a121d8010e9df3ba9ef4c9366c320496
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-05-13 17:26:48 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -32,10 +32,7 @@ class Database:
             caption=None,
             prefix=None,
             suffix=None,
-            sydd=None,
-            syddd=None,
-            topic=None,
-            sydson="True",
+            dump=int(id),
             metadata=False,
             metadata_code=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- " -metadata author="@" -metadata:s:s title="Subtitled By :- @" -metadata:s:a title="By :- @" -metadata:s:v title="By:- @" """
         )
